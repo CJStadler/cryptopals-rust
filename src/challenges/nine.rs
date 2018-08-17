075e169e7959db6e1da267db469bec4ae49a422f
Chris Stadler <chrisstadler@gmail.com>
2018-08-17 12:18:31 -0400

    Fix PKCS padding when message is multiple of block size
    In this case a full block of padding must be added. This way it is
    guaranteed that the value of the last byte is equal to the number of
    padding bytes.

--- src/challenges/nine.rs
+++ src/challenges/nine.rs
@@ -2,13 +2,11 @@ pub fn pkcs_pad(message: &[u8], block_length: usize) -> Vec<u8> {
     let len = message.len();
     let mut padded = message.to_vec();
 
-    if len != block_length {
-        let last_block_length = len % block_length;
-        let padding_size = (block_length - last_block_length) as u8;
+    let last_block_length = len % block_length;
+    let padding_size = (block_length - last_block_length) as u8;
 
-        for _ in 0..padding_size {
-            padded.push(padding_size);
-        }
+    for _ in 0..padding_size {
+        padded.push(padding_size);
     }
 
     padded
@@ -24,7 +22,7 @@ mod tests {
     fn should_pad_the_message_with_the_number_of_padding_bytes() {
         let message = vec![1, 2, 3];
 
-        assert_eq!(message.clone(), pkcs_pad(&message, message.len()));
+        assert_eq!(vec![1, 2, 3, 3, 3, 3], pkcs_pad(&message, 3));
         assert_eq!(vec![1, 2, 3, 1], pkcs_pad(&message, 4));
         assert_eq!(vec![1, 2, 3, 1], pkcs_pad(&message, 2));
         assert_eq!(vec![1, 2, 3, 5, 5, 5, 5, 5], pkcs_pad(&message, 8));
